27f064eca085dcc3119bc8b6ee47c1e85f4a629b
oasislandscapes <62818834+oasislandscapes@users.noreply.github.com>
2020-03-28 20:06:16 -0400

    Add files via upload

---
+++ so-expand-collapsed-sections-by-default.user.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name         SO: Expand Custom Fields and Notes Sections By Default
+// @version      0.2.0
+// @namespace    https://github.com/oasislandscape/
+// @license	 MIT
+// @author       Drew <drew@oasislandscape.com>
+// @description  Jobs and Visits have expandable sections now, and this expands the Custom Field section by default.
+// @run-at	 document-idle
+// @include      /^https?://app\.singleops\.com//
+// @require      http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js
+// @require      https://gist.github.com/raw/2625891/waitForKeyElements.js
+// @updateURL    https://raw.githubusercontent.com/oasislandscape/sous/master/so-expand-collapsed-sections-by-default.user.js
+// @downloadURL  https://raw.githubusercontent.com/oasislandscape/sous/master/so-expand-collapsed-sections-by-default.user.js
+// ==/UserScript==
+
+/* globals jQuery, $, waitForKeyElements */
+
+function expandCustomInputsSection(jNode) {
+    var button = jNode.find('i.job-section-toggle');
+    button.click();
+}
+waitForKeyElements("div.toggle-section.section-custom-inputs:not([aria-expanded])", expandCustomInputsSection);
