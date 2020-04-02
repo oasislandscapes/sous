b27b59ab74e3de4fa79846f1430d238db282775b
oasislandscapes <62818834+oasislandscapes@users.noreply.github.com>
2020-04-01 21:13:15 -0400

    Update so-add-service-buttons.user.js

--- so-add-service-buttons.user.js
+++ so-add-service-buttons.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         SO: Add [Service Visit] and [Spring Turn On] to Visits
 // @namespace    https://github.com/oasislandscape/
-// @version      0.1.0
+// @version      0.1.1
 // @run-at       document-idle
 // @description  This script adds buttons to add service visits and spring turn ons to Visits.
 // @author       Drew <drew@oasislandscape.com>
@@ -18,7 +18,7 @@ function inside_button_group(buttons) {
     return '<div class="btn-group mobile-btn-larger">' + buttons + '</div>';
 }
 function addIrrigationButton(jNode) {
-    var client_id = $("div.row.col-md-3.col-xs-0--no-padding").find('a[href*="/clients/"]').attr('href').replace(/\/clients\/(\d+)/,'$1');
+    var client_id = $("html").find('a[href!="/clients/new"][href*="/clients/"]').attr('href').replace(/\/clients\/(\d+)/,'$1');
 
     var service_visit_html = '<a class="btn-primary btn btn-sm col-xs-6 col-sm-4 col-sm-0" target="_new" href="/jobs/new?client_id=' + client_id + '&amp;template_id=306094&amp;visit_stage_id=4">+ Service Visit</a>';
     var spring_turn_on_html = '<a class="btn-success btn btn-sm col-xs-6 col-sm-4 col-sm-0" target="_new" href="/jobs/new?client_id=' + client_id + '&amp;template_id=360631&amp;visit_stage_id=4">+ Spring Turn-On</a>';
