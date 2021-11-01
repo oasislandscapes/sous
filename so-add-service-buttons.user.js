8dc2c6bb64c618958dbf830182487c999b2acc52
oasislandscapes <62818834+oasislandscapes@users.noreply.github.com>
2021-11-01 10:40:17 -0400

    Update so-add-service-buttons.user.js
    added 2ymp turned on, etc

--- so-add-service-buttons.user.js
+++ so-add-service-buttons.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         SO: Add [BUTTONS] to Visits
 // @namespace    https://github.com/oasislandscape/
-// @version      0.1.4
+// @version      0.1.5
 // @run-at       document-idle
 // @description  This script adds buttons to add service visits and spring turn ons to Visits.
 // @author       Drew <drew@oasislandscape.com>
@@ -19,16 +19,37 @@ function inside_button_group(buttons) {
 }
 function addIrrigationButton(jNode) {
     var client_id = $("html").find('a[href!="/clients/new"][href*="/clients/"]').attr('href').replace(/\/clients\/(\d+)/,'$1');
-    var twoymp_visit_html = '<a class="btn-primary btn btn-sm col-xs-6 col-sm-4 col-sm-0" target="_new" href="/jobs/new?client_id=' + client_id + '&amp;template_id=1800990&amp;visit_stage_id=4">+ 2YMP</a>';
-    var ao_visit_html = '';
-    var chems_visit_html = '';
-    //var ao_visit_html = '<a class="btn-success btn btn-sm col-xs-6 col-sm-4 col-sm-0" target="_new" href="/jobs/new?client_id=' + client_id + '&amp;template_id=559422&amp;visit_stage_id=6">+ A&O</a>';
-    //var chems_visit_html = '<a class="btn-danger btn btn-sm col-xs-6 col-sm-4 col-sm-0" target="_new" href="/jobs/new?client_id=' + client_id + '&amp;template_id=1548434&amp;visit_stage_id=4">+ CHEM</a>';
-    var service_visit_html = '<a class="btn-primary btn btn-sm col-xs-6 col-sm-4 col-sm-0" target="_new" href="/jobs/new?client_id=' + client_id + '&amp;template_id=306094&amp;visit_stage_id=4">+ SVC</a>';
+
+//    var twoymp = '';
+    var twoymp = '<a class="btn-primary btn btn-sm col-xs-6 col-sm-4 col-sm-0" target="_new" href="/jobs/new?client_id=' + client_id + '&amp;template_id=1800990&amp;visit_stage_id=4">+ 2YMP</a>';
+
+    var ao = '';
+//    var ao = '<a class="btn-success btn btn-sm col-xs-6 col-sm-4 col-sm-0" target="_new" href="/jobs/new?client_id=' + client_id + '&amp;template_id=559422&amp;visit_stage_id=6">+ A&O</a>';
+
+//    var flowers = '';
+    var flowers = '<a class="btn-danger btn btn-sm col-xs-6 col-sm-4 col-sm-0" target="_new" href="/jobs/new?client_id=' + client_id + '&amp;template_id=377663&amp;visit_stage_id=6">+ FLOWERS</a>';
+
+    var chems = '';
+//    var chems = '<a class="btn-danger btn btn-sm col-xs-6 col-sm-4 col-sm-0" target="_new" href="/jobs/new?client_id=' + client_id + '&amp;template_id=1548434&amp;visit_stage_id=4">+ CHEM</a>';
+
+//    var pinestraw = '';
+
+    var pinestraw = '<a class="btn-success btn btn-sm col-xs-6 col-sm-4 col-sm-0" target="_new" href="/jobs/new?client_id=' + client_id + '&amp;template_id=306131&amp;visit_stage_id=6">+ PS</a>';
+//    var mulch_brown = '';
+
+    var mulch_brown = '<a class="btn-primary btn btn-sm col-xs-6 col-sm-4 col-sm-0" target="_new" href="/jobs/new?client_id=' + client_id + '&amp;template_id=387863&amp;visit_stage_id=6">+ BROWN</a>';
+//    var mulch_black = '';
+
+    var mulch_black = '<a class="btn-danger btn btn-sm col-xs-6 col-sm-4 col-sm-0" target="_new" href="/jobs/new?client_id=' + client_id + '&amp;template_id=387950&amp;visit_stage_id=6">+ BLACK</a>';
+
+    var service = '<a class="btn-primary btn btn-sm col-xs-6 col-sm-4 col-sm-0" target="_new" href="/jobs/new?client_id=' + client_id + '&amp;template_id=306094&amp;visit_stage_id=4">+ SVC</a>';
+
+//    var sto_pp_html = '';
     var sto_pp_html = '<a class="btn-success btn btn-sm col-xs-6 col-sm-4 col-sm-0" target="_new" href="/jobs/new?client_id=' + client_id + '&amp;template_id=1801036&amp;visit_stage_id=4">+ STO: PP</a>';
+
     var spacer = '&nbsp;&nbsp;';
 
-    var insertion = jNode.find('div').first().prepend(inside_button_group(twoymp_visit_html + ao_visit_html + chems_visit_html + sto_pp_html + service_visit_html + spacer));
+    var insertion = jNode.find('div').first().prepend(inside_button_group(flowers + pinestraw + mulch_brown + mulch_black + twoymp + ao + chems + sto_pp_html + service + spacer));
 
 }
 waitForKeyElements("div.xs-no-float.col-xs-12.col-sm-0.pull-right.job-show-buttons", addIrrigationButton);
